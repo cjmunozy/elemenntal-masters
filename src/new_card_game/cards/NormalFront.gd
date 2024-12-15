caa3b0bffc679c543e04d345cd6f8ea3e70da2b5
cjmunozy <cjmunoz@espol.edu.ec>
2024-12-15 17:32:36 -0500

    Creación de mecánicas del juego

---
+++ src/new_card_game/cards/NormalFront.gd
@@ -0,0 +1,21 @@
+extends "res://src/new_card_game/CardFront.gd"
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	card_labels["Health"] = find_node("Health")
+	
+	card_label_min_sizes["Health"] = Vector2(16,16)
+	
+	original_font_sizes["Health"] = 15
+	# pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
